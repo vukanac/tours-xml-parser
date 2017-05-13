08a4ef76524b03cdb2419efef7c7c3a60c2c0d1a
Vladimir Vukanac <vukanac@gmail.com>
2017-05-13 18:44:40 +0200

    Add XML Departure parser

--- spec/Tour/XmlEntities/DepartureSpec.php
+++ spec/Tour/XmlEntities/DepartureSpec.php
@@ -6,18 +6,68 @@ use Tour\XmlEntities\Departure;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
-
+use SimpleXMLElement;
 use Money\Money;
 
 class DepartureSpec extends ObjectBehavior
 {
+    function let()
+    {
+        $xmlObj = new SimpleXMLElement($this->getXml());
+        $this->beConstructedWith($xmlObj->TOUR->DEP);
+    }
+
     function it_is_initializable()
     {
-        $this->beConstructedWith('x', '100', 10);
         $this->shouldHaveType(Departure::class);
-        $this->getCode()->shouldBe('x');
-        $this->getPrice()->shouldBeLike(Money::EUR('10000'));
-        $this->getDiscount()->shouldBe(0.1);
-        $this->getFinalPrice()->shouldBeLike(Money::EUR('9000'));
     }
+
+    function it_has_method_code()
+    {
+        $this->getCode()->shouldBeString();
+        $this->getCode()->shouldBe('AN-17');
+    }
+
+    function it_has_method_starts()
+    {
+        $this->getStartDate()->shouldBe('04/19/2015');
+    }
+
+    function it_has_method_price()
+    {
+        $this->getPrice()->shouldBe('1724');
+    }
+
+    function it_has_method_discount()
+    {
+        $this->getDiscount()->shouldBe('15');
+    }
+
+    function it_has_method_with_no_discount()
+    {
+        $xmlObj = new SimpleXMLElement($this->getXml());
+        $this->beConstructedWith($xmlObj->TOUR->DEP[2]);
+        $this->getDiscount()->shouldBe('0');
+    }
+
+    public function getXml()
+    {
+        return <<<EOD
+<?xml version="1.0"?>
+<TOURS>
+    <TOUR>
+        <Title><![CDATA[Combo Tour]]></Title>
+        <Code>AE-19</Code>
+        <Duration>18</Duration>
+        <Start>Istanbul</Start>
+        <End>Cairo</End>
+        <Inclusions><![CDATA[Accommodation]]></Inclusions>
+        <DEP DepartureCode="AN-17" Starts="04/19/2015" GBP="1458" EUR="1724" USD="2350" DISCOUNT="15%" />
+        <DEP DepartureCode="AN-18" Starts="04/22/2015" GBP="1558" EUR="1784" USD="2550" DISCOUNT="20%" />
+        <DEP DepartureCode="AN-19" Starts="04/25/2015" GBP="1558" EUR="1784" USD="2550" />
+    </TOUR>
+</TOURS>
+EOD;
+    }
+
 }
--- src/Tour/XmlEntities/Departure.php
+++ src/Tour/XmlEntities/Departure.php
@@ -6,6 +6,8 @@ namespace Tour\XmlEntities;
 use Money\Currencies\ISOCurrencies;
 use Money\Parser\DecimalMoneyParser;
 
+use SimpleXMLElement;
+
 class Departure
 {
     private $code;
@@ -13,59 +15,100 @@ class Departure
     private $discount;
     private $finalPrice;
     
-    public function __construct($code, $price, $discount)
-    {
-        // Money Parser require to parse a string
-        $price = (string) $price;
+    // public function __construct($code, $price, $discount)
+    // {
+    //     // Money Parser require to parse a string
+    //     $price = (string) $price;
 
-        // Code cannot be empty
-        if (empty($code)) {
-            throw new \Exception('Code is required!');
-        }
+    //     // Code cannot be empty
+    //     if (empty($code)) {
+    //         throw new \Exception('Code is required!');
+    //     }
         
-        // Price can be set to 0
-        if (!isset($price) || !is_numeric($price)) {
-            throw new \Exception('Price is not set!');
-        }
+    //     // Price can be set to 0
+    //     if (!isset($price) || !is_numeric($price)) {
+    //         throw new \Exception('Price is not set!');
+    //     }
 
-        // Notify if price is Zero - looks like an error
-        // TODO: Remove this check if not appropriate - if Zero is legitimate
-        if (empty($price)) {
-            throw new \Exception('Price is zero!');
-        }
+    //     // Notify if price is Zero - looks like an error
+    //     // TODO: Remove this check if not appropriate - if Zero is legitimate
+    //     if (empty($price)) {
+    //         throw new \Exception('Price is zero!');
+    //     }
 
-        // Discount must be set, at least to 0
-        if (!isset($discount) || !is_numeric($discount)) {
-            throw new \Exception('Discount is not set to numeric value!');
-        }
+    //     // Discount must be set, at least to 0
+    //     if (!isset($discount) || !is_numeric($discount)) {
+    //         throw new \Exception('Discount is not set to numeric value!');
+    //     }
 
-        $currencies = new ISOCurrencies();
-        $moneyParser = new DecimalMoneyParser($currencies);
-        $money = $moneyParser->parse($price, 'EUR');
+    //     $currencies = new ISOCurrencies();
+    //     $moneyParser = new DecimalMoneyParser($currencies);
+    //     $money = $moneyParser->parse($price, 'EUR');
 
-        $this->code = $code;
-        $this->price = $money;
-        $this->discount = $discount/100;
-        $this->finalPrice = $money->multiply((1 - $this->discount));
-    }
+    //     $this->code = $code;
+    //     $this->price = $money;
+    //     $this->discount = $discount/100;
+    //     $this->finalPrice = $money->multiply((1 - $this->discount));
+    // }
 
-    public function getFinalPrice()
+    // public function getFinalPrice()
+    // {
+    //     return $this->finalPrice;
+    // }
+
+    // public function getDiscount()
+    // {
+    //     return $this->discount;
+    // }
+
+    // public function getPrice()
+    // {
+    //     return $this->price;
+    // }
+
+    // public function getCode()
+    // {
+    //     return $this->code;
+    // }
+
+    /**
+     * Example of xml:
+     *     <DEP DepartureCode="AN-17" Starts="04/19/2015" GBP="1458" EUR="1724" USD="2350" DISCOUNT="15%" />
+     */
+    public function __construct(SimpleXMLElement $oneDepartureXmlObj)
     {
-        return $this->finalPrice;
-    }
-
-    public function getDiscount()
-    {
-        return $this->discount;
-    }
-
-    public function getPrice()
-    {
-        return $this->price;
+        $this->xmlObj = $oneDepartureXmlObj;
     }
 
     public function getCode()
     {
-        return $this->code;
+        return (string) $this->xmlObj['DepartureCode'];
+    }
+
+    public function getStartDate()
+    {
+        return (string) $this->xmlObj['Starts'];
+    }
+
+    public function getPrice()
+    {
+        return (string) $this->xmlObj['EUR'];
+    }
+
+    public function getDiscount()
+    {
+        // with discount:
+        // <DEP DepartureCode="AN-17" Starts="04/19/2015" GBP="1458" EUR="1724" USD="2350" DISCOUNT="15%" />
+        // without discount
+        // <DEP DepartureCode="AN-19" Starts="04/25/2015" GBP="1558" EUR="1784" USD="2550" />
+        
+        if (!isset($this->xmlObj['DISCOUNT'])) {
+            return '0';
+        }
+
+        $str = (string) $this->xmlObj['DISCOUNT'];
+        $str = trim($str);
+        $str = rtrim($str, '%');
+        return $str;
     }
 }
